93bca5a09bf64ccc9ba625edecf8075781dde5da
alvaro <agomez@yepcode.io>
2025-06-26 14:32:58 +0200

    Add YepCode Storage sdk

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { YepCodeRun, Execution } from "./run";
 export { YepCodeEnv } from "./env";
 export * from "./api";
+export * from "./storage";
 export * from "./types";
---
+++ src/storage/yepcodeStorage.ts
@@ -0,0 +1,30 @@
+import { YepCodeApi, YepCodeApiManager } from "../api";
+import { StorageObject, YepCodeApiConfig } from "../api/types";
+import { Readable } from "stream";
+
+export class YepCodeStorage {
+  private api: YepCodeApi;
+
+  constructor(config: YepCodeApiConfig = {}) {
+    this.api = YepCodeApiManager.getInstance(config);
+  }
+
+  async upload(
+    filename: string,
+    file: File | Blob | Readable
+  ): Promise<StorageObject> {
+    return this.api.createObject({ name: filename, file });
+  }
+
+  async list(): Promise<StorageObject[]> {
+    return this.api.getObjects();
+  }
+
+  async download(filename: string): Promise<Readable> {
+    return this.api.getObject(filename);
+  }
+
+  async delete(filename: string): Promise<void> {
+    await this.api.deleteObject(filename);
+  }
+}
